530d6a6f15ba190c23c34f899be37b928b2cd34b
Karan <144516544+kaulkaran@users.noreply.github.com>
2025-07-13 09:30:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from PIL import Image
 import io 
 import os
 
+
 app = Flask(__name__) 
 UPLOAD_FOLDER = 'static' 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -34,4 +35,5 @@ def download():
     return send_file(output_path, as_attachment=True)
 
 if __name__ == '__main__': 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
